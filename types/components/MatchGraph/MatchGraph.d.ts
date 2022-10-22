077127330947eff61e0bf1ca0b97b9cbc3efa299
kurata321 <kurata.hironori321@gmail.com>
2022-10-23 00:29:19 +0900

    update build

---
+++ types/components/MatchGraph/MatchGraph.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+import './MatchGraph.css';
+declare type Props = {
+    name: string;
+};
+declare const _default: import("react").MemoExoticComponent<(props: Props) => JSX.Element | null>;
+export default _default;
